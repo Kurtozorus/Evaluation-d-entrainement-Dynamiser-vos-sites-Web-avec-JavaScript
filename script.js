20a3c9109b9c48abf863519cd3b06e15624eb303
Kurtozorus <olivier.marchand26@hotmail.fr>
2023-12-04 18:55:47 +0100

    Derniere retouche

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ let facesOfDice = [
 let newGame = document.getElementById('new-game');
 let rollDice = document.getElementById('roll');
 let hold = document.getElementById('hold');
-let turnoff = document.getElementsByClassName('disabled')
 let dote1 = document.getElementById('dot1');
 let dote2 = document.getElementById('dot2');
-let globaleScore1 = document.getElementById('globalsc1');
-let globaleScore2 = document.getElementById('globalsc2');
+let GLOBAL1Score = document.getElementById('globalsc1');
+let GLOBAL2Score = document.getElementById('globalsc2');
 let ROUND1Score = document.getElementById('score1');
 let ROUND2Score = document.getElementById('score2');
 let current1 = 0;
@@ -24,13 +23,12 @@ let global1 = 0;
 let global2 = 0;
 let round = 0;
 
-// function dotActive {
-
-// }
+// Evite que le joueur 1 passe son tour en début de partie.
+document.querySelector('#hold').disabled = true;
 
 function roll()  {
-    //boucle qui récupére l'élément image de notre html
-    //Puis animation "Shake" pendant une seconde
+    // Boucle qui récupére l'élément image de notre code html
+    // Puis animation "Shake" pendant une seconde
     dice.forEach(function (dic) {
         dic.classList.add("shake");
     });
@@ -39,56 +37,67 @@ function roll()  {
             dic.classList.remove("shake")
         })
     }, 1000)
-    //Récupération d'une valeur (aléatoire entre 1 et 6)
-    let dicvalue = Math.floor(Math.random() * 6)
-    console.log(dicvalue + 1)
-    //Attribution de la valeur à l'image du dé
-    document.querySelector("#dic").setAttribute("src", facesOfDice[dicvalue]);
-    //Récupération de la valeur du dé à l'affichage des round
-    diceResult = dicvalue + 1;
+    // Récupération d'une valeur (aléatoire entre 1 et 6, 6 étant le nombre de face du dé possible)
+    let diceValue = Math.floor(Math.random() * 6)
+    console.log(diceValue + 1)
+    // Attribution de la valeur(aléatoire) à l'image du dé 
+    document.querySelector("#dic").setAttribute("src", facesOfDice[diceValue]);
+    // Récupération de la valeur du dé
+    diceResult = diceValue + 1;
 
     if(round == 0) {
-        //Lancer les dés joueur 1
+        // Lancer du dé joueur 1
+        // On récupére le résultat dans une variable et on l'ajoute l'affichage
         current1 = diceResult;
         ROUND1Score.innerHTML = current1;
-        //On réactive le bouton hold par son id (pourde nouveau appuyer sur le boutton hold)
+        // On réactive le bouton hold par son id (pourde nouveau appuyer sur le boutton hold)
         document.querySelector('#hold').disabled = false;
     }else {
-        //Lancer les dés joueur 2
+        // Lancer les dés joueur 2
+        // On récupére le résultat dans une variable et on l'ajoute l'affichage
         current2 = diceResult;
         ROUND2Score.innerHTML = current2;
-        //On réactive le bouton hold par son id (pourde nouveau appuyer sur le boutton hold)
+        // On réactive le bouton hold par son id (pourde nouveau appuyer sur le boutton hold)
         document.querySelector('#hold').disabled = false;
     }
     //Condition si le joueur fait 1, il passe son tour et on ne compte pas le point
     if(diceResult == 1 & round == 0) {
+        // On réinitialise le résultat, la variable ainsi que l'affichage
         diceResult = 0;
         current1 = 0;
         ROUND1Score.innerHTML = 0;
+        // Change l'affichage du joueur et passe au joueur 2
         dote1.classList.add('dot1d');
         dote2.classList.remove('dot2d');
+        // On désactive le boutton hold (pour êviter un switch de joueur) 
+        document.querySelector('#hold').disabled = true;
+        // Et on passe le tour.
         round++;
     }else if(diceResult == 1 & round == 1){
+        // On réinitialise le résultat, la variable ainsi que l'affichage
         diceResult = 0;
         current2 = 0;
         ROUND2Score.innerHTML = 0;
+        // Change l'affichage du joueur et passe au joueur 1
         dote2.classList.add('dot2d');
         dote1.classList.remove('dot1d');
+        // On désactive le boutton hold (pour êviter un switch de joueur) 
+        document.querySelector('#hold').disabled = true;
+        // Et on passe le tour.
         round--;
     }
 }
-//Appel de la fonction roll sur le bouton rollDice
 
+//Appel de la fonction roll sur le bouton rollDice
 rollDice.addEventListener('click', () =>{
     roll()
 });
-console.log(dice)
 
 function keephold() {
     if(round == 0) {
         //Récupére le résultat du dé (diceResult) on l'ajoute a global puis on l'affiche dans globale1
         global1 += diceResult;
-        globaleScore1.innerHTML = global1;
+        GLOBAL1Score.innerHTML = global1;
         ROUND1Score.innerHTML = 0;
         diceResult = 0;
         //Change l'affichage du joueur et passe au joueur 2
@@ -101,7 +110,7 @@ function keephold() {
     }else {
         //Récupére le résultat du dé (diceResult) on l'ajoute a global puis on l'affiche dans globale2
         global2 += diceResult;
-        globaleScore2.innerHTML = global2;
+        GLOBAL2Score.innerHTML = global2;
         ROUND2Score.innerHTML = 0;
         diceResult = 0;
         //Change l'affichage du joueur et passe au joueur 1
@@ -110,7 +119,18 @@ function keephold() {
         //On désactive le bouton hold par son id (pour éviter de pouvoir appuyer de nouveau)
         document.querySelector('#hold').disabled = true;
         //change de tour -1
-        round--
+        round-- 
+    }
+    // Fin de partie limité à 100 point
+    // On bloque les bouttons hold et roll 
+    if(global1 >= 100) {
+        alert('Le Player 1 remporte la partie !')
+        document.querySelector('#hold').disabled = true;
+        document.querySelector('#roll').disabled = true;
+    }else if (global2 >= 100) {
+        alert(' Le Player 2 remporte la partie !')
+        document.querySelector('#hold').disabled = true;
+        document.querySelector('#roll').disabled = true;
     }
 }
 //Appel de la fonction hold
@@ -122,13 +142,20 @@ function restart() {
     //On remet toutes les valeurs à 0 et l'affichage du joueur qui commence
     ROUND1Score.innerHTML = 0;
     ROUND2Score.innerHTML = 0;
-    globaleScore1.innerHTML = 0;
-    globaleScore2.innerHTML = 0;
+    GLOBAL1Score.innerHTML = 0;
+    GLOBAL2Score.innerHTML = 0;
     global1 = 0;
     global2 = 0;
     round = 0;
+    // Remise de l'affichage sur le joueur 1
     dote2.classList.add('dot2d');
     dote1.classList.remove('dot1d');
+    // On bloque le boutton hold pour éviter que le joueur 1 passe son tour
+    document.querySelector('#hold').disabled = true;
+    // Tout en débloquant le boutton roll pour qu'il puisse lancer le dé
+    document.querySelector('#roll').disabled = false;
+    // Remise a l'affichage de la première face du dé.
+    document.querySelector("#dic").setAttribute("src", facesOfDice[0]);
 }
 //Appel de la fonction restart
 newGame.addEventListener('click', () => {
